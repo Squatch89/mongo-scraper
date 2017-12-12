d026e2c717207d4f6b0b1e7cdccd98a11960fcae
Gene Williams <GeneWillb@Gmail.com>
2017-12-12 12:19:37 -0500

    added scraper route

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
+
+app.get("/scrape", function(req,res) {
+    request("", function(error, response, html) {
+        const $ = cheerio.load(html);
+        
+        $(".headline--wrapper").each(function(i, element) {
+            const headline = $(element).children("a").text();
+            const summary = $(element).children("p").text();
+            const link = $(element).children("a").attr("href");
+            
+            if (headline && summary && link) {
+                //insert into db
+            }
+        })
+    });
+    res.send("scrape complete");
+});
+
+
 app.listen(PORT, function () {
     console.log(`App running on port ${PORT}!`);
 });
